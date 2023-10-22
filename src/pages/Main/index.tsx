c48fbab4c6e89333b066148ca58a99bb44aaab3a
Kurul-afk <arsenshergaziev@gmail.com>
2023-10-22 13:38:04 +0600

    312

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <>
+      <div style={{ background: "purple", height: "600px" }}>
+        <h1>Main</h1>
+      </div>
+      <div style={{ background: "purple", height: "600px" }}>
+        <h2>Second</h2>
+      </div>
+    </>
+  );
 };
 
 export default Main;
